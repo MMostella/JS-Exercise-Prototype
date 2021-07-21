41c8b695d684998c6a055c77a31ed110eda96a58
MMostella <masonmostella@gmail.com>
2021-07-21 15:18:04 -0500

    Finished

--- index.js
+++ index.js
@@ -39,13 +39,42 @@ Airplane.prototype.land = function () {
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-function Person() {
-  
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+  this.stomach = [];
 }
 
+Person.prototype.eat = function(edible){
+  if (this.stomach.length < 10){
+    this.stomach.push(edible);
+  }
+}
 
+Person.prototype.poop = function(){
+  this.stomach = [];
+}
 
+Person.prototype.toString = function(){
+  return `${this.name}, ${this.age}`;
+}
 
+const mason = new Person('Mason', 26);
+const allison = new Person('Allison', 22);
+
+mason.eat('Tacos');
+mason.eat('Pizza');
+mason.eat('Sushi');
+mason.eat('Pasta');
+mason.eat('Burger');
+
+console.log(mason.stomach);
+
+mason.poop();
+
+console.log(mason.stomach);
+
+console.log(mason.toString());
 
 
 
@@ -63,10 +92,27 @@ function Person() {
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-function Car() {
-  
+function Car(model, milesPerGallon) {
+  this.model = model;
+  this.milesPerGallon = milesPerGallon;
+  this.tank = 0;
+  this.odometer = 0;
 }
 
+Car.prototype.fill = function(gallons){
+  return this.tank += gallons;
+}
+
+
+const jeep = new Car('Jeep', 50);
+
+jeep.fill(30);
+
+console.log(jeep);
+
+// Car.prototype.drive = function(distance){
+
+// }
 
 /*
   TASK 3
@@ -75,18 +121,35 @@ function Car() {
     - Besides the methods on Person.prototype, babies have the ability to `.play()`:
         + Should return a string "Playing with x", x being the favorite toy.
 */
-function Baby() {
- 
-}
+function Baby(name, age, favoriteToy) {
+  Person.call(this, name, age)
+  this.favoriteToy = favoriteToy;
+};
+
+Baby.prototype = Object.create(Person.prototype);
+
+Baby.prototype.play = function(){
+  return `Playing with ${this.favoriteToy}`;
+};
+
+const amelia = new Baby('Amelia', 2, 'Trampoline')
+
+console.log(amelia);
+amelia.eat('Noodles');
+console.log(amelia.stomach);
+amelia.poop();
+console.log(amelia.stomach);
+console.log(amelia.toString());
+console.log(amelia.play());
 
 
 /* 
   TASK 4
   In your own words explain the four principles for the "this" keyword below:
-  1. 
-  2. 
-  3. 
-  4. 
+  1. New Binding
+  2. Implicit
+  3. Explicit
+  4. Window
 */
 
 
